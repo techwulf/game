677fd085bc6946a62f2833aafd684ea18248f3c7
Master Foo <masterfoo@aspire-v5.Home>
2016-12-01 17:43:10 -0700

    add translate to avatar_model

--- source/avatar/avatar_controller.py
+++ source/avatar/avatar_controller.py
@@ -8,14 +8,17 @@ class AvatarController:
     def __init__(self):
         pass
 
-    def move(self):
+    def on_loop(self):
         if keyboard.KEYBOARD.L_SHIFT:
             self.run()
         else:
             self.walk()
 
-        if keyboard.KEYBOARD.UP == False and keyboard.KEYBOARD.RIGHT == False and keyboard.KEYBOARD.DOWN == False and keyboard.KEYBOARD.LEFT == False:
-            self.stand()
+        if keyboard.KEYBOARD.UP == False:
+            if keyboard.KEYBOARD.RIGHT == False:
+                if keyboard.KEYBOARD.DOWN == False:
+                    if keyboard.KEYBOARD.LEFT == False:
+                        self.stand()
 
         if keyboard.KEYBOARD.UP == True:
             self.direction = global_variables.NORTH
@@ -44,22 +47,5 @@ class AvatarController:
                 self.direction = global_variables.NORTHWEST
             elif keyboard.KEYBOARD.DOWN == True:
                 self.direction = global_variables.SOUTHWEST
-        
-        self.position.x = (
-            self.position.x + self.velocity() * math.sin(math.radians(self.direction))
-        )
-        self.position.y = (
-            self.position.y + -self.velocity() * math.cos(math.radians(self.direction))
-        )
 
-    def run(self):
-        self.velocity_modifier = 2
-
-    def walk(self):
-        self.velocity_modifier = 1
-
-    def stand(self):
-        self.velocity_modifier = 0
-
-    def on_loop(self):
-        self.move()
+        self.translate()
