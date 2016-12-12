462f5c5c3526eba90a5ef1ea62ec899e5e112e45
Master Foo <Master.Foo.v.1.0.0@gmail.com>
2016-12-11 23:13:44 -0700

    Clean up AvatarController

--- source/entities/avatar/controller.py
+++ source/entities/avatar/controller.py
@@ -8,51 +8,72 @@ class Controller(controller.Controller):
         pass
 
     def on_loop(self):
+        self.running()
+        self.standing()
+        self.moving_north()
+        self.moving_east()
+        self.moving_south()
+        self.moving_west()
+        self.grabbing_item()
+
+        self.translate()
+        pass
+
+    def running(self):
         if keyboard.KEYBOARD.L_SHIFT:
             self.run()
         else:
             self.walk()
 
+    def standing(self):
         if keyboard.KEYBOARD.UP == False:
             if keyboard.KEYBOARD.RIGHT == False:
                 if keyboard.KEYBOARD.DOWN == False:
                     if keyboard.KEYBOARD.LEFT == False:
                         self.stand()
+        pass
 
+    def moving_north(self):
         if keyboard.KEYBOARD.UP == True:
             self.direction = global_variables.NORTH
             if keyboard.KEYBOARD.RIGHT == True:
                 self.direction = global_variables.NORTHEAST
             elif keyboard.KEYBOARD.LEFT == True:
                 self.direction = global_variables.NORTHWEST
-        
+        pass
+
+    def moving_east(self):
         if keyboard.KEYBOARD.RIGHT == True:
             self.direction = global_variables.EAST
             if keyboard.KEYBOARD.UP == True:
                 self.direction = global_variables.NORTHEAST
             elif keyboard.KEYBOARD.DOWN == True:
                 self.direction = global_variables.SOUTHEAST
-        
+        pass
+
+    def moving_south(self):
         if keyboard.KEYBOARD.DOWN == True:
             self.direction = global_variables.SOUTH
             if keyboard.KEYBOARD.RIGHT == True:
                 self.direction = global_variables.SOUTHEAST
             elif keyboard.KEYBOARD.LEFT == True:
                 self.direction = global_variables.SOUTHWEST
-        
+        pass
+
+
+    def moving_west(self):
         if keyboard.KEYBOARD.LEFT == True:
             self.direction = global_variables.WEST
             if keyboard.KEYBOARD.UP == True:
                 self.direction = global_variables.NORTHWEST
             elif keyboard.KEYBOARD.DOWN == True:
                 self.direction = global_variables.SOUTHWEST
+        pass
 
+    def grabbing_item(self):
         if keyboard.KEYBOARD.RETURN == True:
             self.holding = self.parent.rock
 
         if keyboard.KEYBOARD.RETURN == False:
             self.holding = None
-        pass
-
-        self.translate()
         pass
