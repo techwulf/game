a9d235ccff3601c67c67f36a121d1d338f33b799
techwulf <t.zach.moore@gmail.com>
2016-12-18 15:37:51 -0700

    Create a Turnip #28

---
+++ source/entities/turnip/animation_config/damaged_picked.py
@@ -0,0 +1,19 @@
+damaged_picked_data = {
+    "entity_name" : "turnip",
+    "action" : "damaged_picked",
+    "animations" : [
+        {
+            "direction" : "north",
+            "frames" : [('000.png', 0.1)]
+        },{
+            "direction" : "east",
+            "frames" : [('000.png', 0.1)]
+        },{
+            "direction" : "south",
+            "frames" : [('000.png', 0.1)]
+        },{
+            "direction" : "west",
+            "frames" : [('000.png', 0.1)]
+        }
+    ]
+}
---
+++ source/entities/turnip/animation_config/damaged_ripe.py
@@ -0,0 +1,19 @@
+damaged_ripe_data = {
+    "entity_name" : "turnip",
+    "action" : "damaged_ripe",
+    "animations" : [
+        {
+            "direction" : "north",
+            "frames" : [('000.png', 0.1)]
+        },{
+            "direction" : "east",
+            "frames" : [('000.png', 0.1)]
+        },{
+            "direction" : "south",
+            "frames" : [('000.png', 0.1)]
+        },{
+            "direction" : "west",
+            "frames" : [('000.png', 0.1)]
+        }
+    ]
+}
